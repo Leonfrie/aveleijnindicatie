b73fdd31952aac48c400bf011ab72f0791e3a087
jordyortega <jordy-9999@hotmail.com>
2018-12-04 13:19:01 +0100

    test

---
+++ MVCLeegProject/Controllers/FilterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLeegProject.Controllers
+{
+    public class FilterController : Controller
+    {
+        // GET: Filter
+        public ActionResult Index(int type)
+        {
+            return View();
+        }
+    }
+}
